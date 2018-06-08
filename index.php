b3b13e6202e525a0e5b0d52cf99d5820aa2a954e
Karthick <karthi.karthi47@gmail.com>
2018-06-08 17:10:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+li {
+    display: inline;
+}
+</style>
+</head>
+<body>
+
+
+<ul>
+  <li><a href="index">Home</a></li>
+  <li><a href="news">News</a></li>
+  <li><a href="contact">Contact</a></li>
+  <li><a href="about">About</a></li>
+</ul> 
+
+<div> Welcome to home page...  </div>
+
+
+</body>
+</html>
